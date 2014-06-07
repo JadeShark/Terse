da1f71728964f967412b92b33a7b4c903d44df20
aBigWrench <abigwrench@outlook.com>
2014-06-07 10:41:04 -0400

    First upload of underscores theme

---
+++ content-single.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package Terse
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+			<?php terse_posted_on(); ?>
+		</div><!-- .entry-meta -->
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'terse' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php
+			/* translators: used between list items, there is a space after the comma */
+			$category_list = get_the_category_list( __( ', ', 'terse' ) );
+
+			/* translators: used between list items, there is a space after the comma */
+			$tag_list = get_the_tag_list( '', __( ', ', 'terse' ) );
+
+			if ( ! terse_categorized_blog() ) {
+				// This blog only has 1 category so we just need to worry about tags in the meta text
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'terse' );
+				} else {
+					$meta_text = __( 'Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'terse' );
+				}
+
+			} else {
+				// But this blog has loads of categories so we should probably display them here
+				if ( '' != $tag_list ) {
+					$meta_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'terse' );
+				} else {
+					$meta_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" rel="bookmark">permalink</a>.', 'terse' );
+				}
+
+			} // end check for categories on this blog
+
+			printf(
+				$meta_text,
+				$category_list,
+				$tag_list,
+				get_permalink()
+			);
+		?>
+
+		<?php edit_post_link( __( 'Edit', 'terse' ), '<span class="edit-link">', '</span>' ); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
